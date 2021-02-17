1b8a06e8b39b4911fb0a03b11dfad72d49e1b2c5
Ghazi Yusaf <ghaziyusaf@gmail.com>
2021-02-17 00:02:41 +0000

    Add average output
    Average CO2, temperature and humidity are now output to the console
    for the specified data file.
    
    Signed-off-by: Ghazi Yusaf <ghaziyusaf@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-import json
+import json, sys, fileinput
 
-with open('data/2021-01-17 22 05.txt') as dataText:
+print('Enter the path of the data file to read:')
+
+with open(input()) as dataText:
     data = json.load(dataText)
 
-print(data['data']['2021-01-10 07:20:00 UTC']['944']['co2'])
+startDate = data['data_range']['minimum_date'].replace('T', ' ').split('.')[0]
+endDate = data['data_range']['maximum_date'].replace('T', ' ').split('.')[0]
+
+totalTemperature = 0
+totalHumidity = 0
+totalCO2 = 0
+count = 0
+
+for (k, v) in data['data'].items():
+    count += 1
+    totalTemperature += data['data'][f'{k}']['944']['temperature']
+    totalHumidity += data['data'][f'{k}']['944']['humidity']
+    totalCO2 += data['data'][f'{k}']['944']['co2']
+
+averageTemperature = totalTemperature / count
+averageHumidity = totalHumidity / count
+averageCO2 = totalCO2 / count
+
+print(f'\nAverage Temperature: {averageTemperature}\nAverage Humidity: {averageHumidity}\nAverage CO2: {averageCO2}')
+
+# print(f'enter a time frame between {startDate} and {endDate} to produce output:')
+
+# print('Start:')
+# userStart = input()
+
+# print('End:')
+# userEnd = input()
+
+# temperature, humidity, co2.
